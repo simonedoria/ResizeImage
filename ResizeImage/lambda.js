8311205d3241a239c61865c1092015ce50331f11
simonedoria <simonedoriasd@gmail.com>
2018-07-15 10:34:55 +0200

    Commit 5

--- ResizeImage/lambda.js
+++ ResizeImage/lambda.js
@@ -1,16 +1,16 @@
+let AWS = require('aws-sdk');
 'use strict'
 
 
-const AWS = require('aws-sdk')
-const S3 = new AWS.S3({signatureVersion: 'v4'});
+const S3 = new AWS.S3({ signatureVersion: 'v4' });
 const Sharp = require('sharp');
 const PathPattern = new RegExp("(.*/)?(.*)/(.*)");
 
 // parameters
-const {BUCKET, URL} = process.env
+const URL = "http://infooggi.s3-website.eu-central-1.amazonaws.com";
 
 
-exports.handler = function(event, _context, callback) {
+exports.handler = function (event, _context, callback) {
     var path = event.queryStringParameters.path;
     var parts = PathPattern.exec(path);
     var dir = parts[1] || '';
@@ -22,16 +22,18 @@ exports.handler = function(event, _context, callback) {
     var func = options.length > 1 ? options[1] : null;
 
     var contentType;
-    S3.getObject({Bucket: BUCKET, Key: dir + filename})
-        .promise()
+    S3.getObject({
+        'Bucket': "infooggi",
+        'Key': dir + filename
+    }).promise()
         .then(data => {
             contentType = data.ContentType;
             var img = Sharp(data.Body)
                 .resize(
-                    sizes[0] === 'AUTO' ? null : parseInt(sizes[0]),
-                    sizes[1] === 'AUTO' ? null : parseInt(sizes[1]));
+                sizes[0] === 'AUTO' ? null : parseInt(sizes[0]),
+                sizes[1] === 'AUTO' ? null : parseInt(sizes[1]));
 
-            switch (func){
+            switch (func) {
                 case 'max': img = img.max(); break;
                 case 'min': img = img.min(); break;
                 case null: break;
@@ -39,33 +41,32 @@ exports.handler = function(event, _context, callback) {
                     callback(null, {
                         statusCode: 400,
                         body: `Unknown func parameter "${func}"\n` +
-                              'For query ".../150x150_func", "_func" must be either empty, "_min" or "_max"',
-                        headers: {"Content-Type": "text/plain"}
+                        'For query ".../150x150_func", "_func" must be either empty, "_min" or "_max"',
+                        headers: { "Content-Type": "text/plain" }
                     })
-                    return new Promise(() => {})  // the next then-blocks will never be executed
+                    return new Promise(() => { })  // the next then-blocks will never be executed
             }
 
             return img.withoutEnlargement().toBuffer();
-        })
-        .then(result =>
+        }).then(result =>
             S3.putObject({
-                Body: result,
-                Bucket: BUCKET,
+                "Body": result,
+                "Bucket": "infooggi",
                 ContentType: contentType,
-                Key: path
+                "Key": path
             }).promise()
         )
         .then(() =>
             callback(null, {
                 statusCode: 301,
-                headers: {"Location" : `${URL}/${path}`}
+                headers: { "Location": `${URL}/${path}` }
             })
         )
         .catch(e => {
             callback(null, {
                 statusCode: e.statusCode || 400,
                 body: 'Exception: ' + e.message,
-                headers: {"Content-Type": "text/plain"}
+                headers: { "Content-Type": "text/plain" }
             })
         });
 }
