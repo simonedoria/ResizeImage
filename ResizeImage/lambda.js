0e6bbabcbb08abd25c5d579b736193227cdd8fe0
simonedoria <simonedoriasd@gmail.com>
2018-07-15 10:14:45 +0200

    Commit 4

--- ResizeImage/lambda.js
+++ ResizeImage/lambda.js
@@ -1,146 +1,71 @@
-// dependencies
-var async = require('async');
-var AWS = require('aws-sdk');
-var gm = require('gm').subClass({
-  imageMagick: true
-}); // use ImageMagick
-var util = require('util');
-
-// configuration as code - add, modify, remove array elements as desired
-var imgVariants = [
-  {
-    "SIZE": "Large",
-    "POSTFIX": "-l",
-    "MAX_WIDTH": 1200,
-    "MAX_HEIGHT": 1000,
-    "SIZING_QUALITY": 70,
-    "INTERLACE": "Line"
-  },
-  {
-    "SIZE": "Medium",
-    "POSTFIX": "-m",
-    "MAX_WIDTH": 800,
-    "MAX_HEIGHT": 800,
-    "SIZING_QUALITY": 60,
-    "INTERLACE": "Line"
-  },
-  {
-    "SIZE": "Small",
-    "POSTFIX": "-s",
-    "MAX_WIDTH": 300,
-    "MAX_HEIGHT": 400,
-    "SIZING_QUALITY": 50,
-    "INTERLACE": "Line"
-  }
-];
-// The name of the destination S3 bucket is derived by adding this postfix to the name of the source S3 bucket:
-var DST_BUCKET_POSTFIX = "";
+'use strict'
 
 
+const AWS = require('aws-sdk')
+const S3 = new AWS.S3({signatureVersion: 'v4'});
+const Sharp = require('sharp');
+const PathPattern = new RegExp("(.*/)?(.*)/(.*)");
 
-// get reference to S3 client
-var s3 = new AWS.S3();
-
-exports.handler = function (event, context) {
-  // Read options from the event.
-  console.log("Reading options from event:\n", util.inspect(event, {
-    depth: 5
-  }));
-  var srcBucket = event.Records[0].s3.bucket.name;
-  // Object key may have spaces or unicode non-ASCII characters.
-  var srcKey = decodeURIComponent(event.Records[0].s3.object.key.replace(/\+/g, " "));
-  // derive the file name and extension
-  var srcFile = srcKey.match(/(.+)\.([^.]+)/);
-  var srcName = srcFile[1];
-  var scrExt = srcFile[2];
-  // set the destination bucket
-  var dstBucket = srcBucket + DST_BUCKET_POSTFIX;
+// parameters
+const {BUCKET, URL} = process.env
 
 
-  // make sure that source and destination are different buckets.
-  if (srcBucket === dstBucket) {
-    console.error("Destination bucket must be different from source bucket.");
-    return;
-  }
+exports.handler = function(event, _context, callback) {
+    var path = event.queryStringParameters.path;
+    var parts = PathPattern.exec(path);
+    var dir = parts[1] || '';
+    var options = parts[2].split('_');
+    var filename = parts[3];
 
-  if (!scrExt) {
-    console.error('unable to derive file type extension from file key ' + srcKey);
-    return;
-  }
 
-  if (scrExt != "jpg" && scrExt != "png") {
-    console.log('skipping non-supported file type ' + srcKey + ' (must be jpg or png)');
-    return;
-  }
+    var sizes = options[0].split("x");
+    var func = options.length > 1 ? options[1] : null;
 
-  function processImage(data, options, callback) {
-    gm(data.Body).size(function (err, size) {
+    var contentType;
+    S3.getObject({Bucket: BUCKET, Key: dir + filename})
+        .promise()
+        .then(data => {
+            contentType = data.ContentType;
+            var img = Sharp(data.Body)
+                .resize(
+                    sizes[0] === 'AUTO' ? null : parseInt(sizes[0]),
+                    sizes[1] === 'AUTO' ? null : parseInt(sizes[1]));
 
-      var scalingFactor = Math.min(
-        options.MAX_WIDTH / size.width,
-        options.MAX_HEIGHT / size.height
-      );
-      var width = scalingFactor * size.width;
-      var height = scalingFactor * size.height;
+            switch (func){
+                case 'max': img = img.max(); break;
+                case 'min': img = img.min(); break;
+                case null: break;
+                default:
+                    callback(null, {
+                        statusCode: 400,
+                        body: `Unknown func parameter "${func}"\n` +
+                              'For query ".../150x150_func", "_func" must be either empty, "_min" or "_max"',
+                        headers: {"Content-Type": "text/plain"}
+                    })
+                    return new Promise(() => {})  // the next then-blocks will never be executed
+            }
 
-      this.resize(width, height)
-        .quality(options.SIZING_QUALITY || 75)
-        .interlace(options.INTERLACE || 'None')
-        .toBuffer(scrExt, function (err, buffer) {
-          if (err) {
-            callback(err);
-          } else {
-            uploadImage(data.ContentType, buffer, options, callback);
-          }
+            return img.withoutEnlargement().toBuffer();
+        })
+        .then(result =>
+            S3.putObject({
+                Body: result,
+                Bucket: BUCKET,
+                ContentType: contentType,
+                Key: path
+            }).promise()
+        )
+        .then(() =>
+            callback(null, {
+                statusCode: 301,
+                headers: {"Location" : `${URL}/${path}`}
+            })
+        )
+        .catch(e => {
+            callback(null, {
+                statusCode: e.statusCode || 400,
+                body: 'Exception: ' + e.message,
+                headers: {"Content-Type": "text/plain"}
+            })
         });
-    });
-  }
-
-  function uploadImage(contentType, data, options, callback) {
-    // Upload the transformed image to the destination S3 bucket.
-    s3.putObject({
-        Bucket: dstBucket,
-        Key: srcName + options.POSTFIX + '.' + scrExt,
-        Body: data,
-        ContentType: contentType
-      },
-      callback);
-  }
-
-
-  // Download the image from S3 and process for each requested image variant.
-  async.waterfall(
-    [
-      function download(next) {
-          // Download the image from S3 into a buffer.
-          s3.getObject({
-              Bucket: srcBucket,
-              Key: srcKey
-            },
-            next);
-      },
-      function processImages(data, next) {
-          async.each(imgVariants, function (variant, next) {
-            processImage(data, variant, next);
-          }, next);
-      }
-
-    ],
-    function (err) {
-      if (err) {
-        console.error(
-          'Unable to resize ' + srcBucket + '/' + srcKey +
-          ' and upload to ' + dstBucket +
-          ' due to an error: ' + err
-        );
-      } else {
-        console.log(
-          'Successfully resized ' + srcBucket + '/' + srcKey +
-          ' and uploaded to ' + dstBucket
-        );
-      }
-
-      context.done();
-    }
-  );
-};
+}
